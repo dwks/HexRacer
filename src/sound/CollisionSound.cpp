fb6d66b8fecbc7902ba2e9b5eabbd5cb3edde755
Andrew Spittal <aspittal@gmail.com>
2011-03-05 18:26:34 -0700

    Added a couple more collision sounds

--- src/sound/CollisionSound.cpp
+++ src/sound/CollisionSound.cpp
@@ -26,9 +26,12 @@ void CollisionSound::initialize(){
     ALfloat sourceVelocity[] = {0.0, 0.0, 0.0};
     
     //Load the engine sound into the buffers
-    string file = "data/sound/soundfx/collision01.wav";
+    string file[] = {"data/sound/soundfx/collision01.wav",
+        "data/sound/soundfx/collision02.wav",
+        "data/sound/soundfx/collision03.wav"
+    };
     for(int i=0;i<16;i++){
-        ALHelpers::loadFileToBuffer(collisionBuffers[i],file);
+        ALHelpers::loadFileToBuffer(collisionBuffers[i],file[i%3]);
         
         // bind a sound source to the buffer data
         ALHelpers::bindBufferToSource(collisionBuffers[i], collisionSources[i]);
@@ -47,7 +50,7 @@ void CollisionSound::cleanUp(){
     ALHelpers::destroySources(collisionSources,16);
 }
 void CollisionSound::playCollision(Math::Point location){
-    if((location-lastLocation).length()>1.0){
+    if((location-lastLocation).length()>2.0){
         ALfloat sourcePosition[] = {
             location.getX(), 
             location.getY(), 
